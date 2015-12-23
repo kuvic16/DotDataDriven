da93faf3ae0c79be247f4ac93095c35d79df8045
Shaiful Islam <kuvic16@gmail.com>
2015-12-23 16:51:44 +0600

    minor changes made

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -3,51 +3,54 @@
 /* @var $model LoginForm */
 /* @var $form CActiveForm  */
 
-$this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
-	'Login',
+$this->pageTitle = Yii::app()->name . ' - Login';
+$this->breadcrumbs = array(
+    'Login',
 );
 ?>
-
 <h1>Login</h1>
+<hr>
+<div class="row">
+    <div class="col-md-3">
+        <?php
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'login-form',
+            'enableClientValidation' => true,
+            'clientOptions' => array(
+                'validateOnSubmit' => true,
+            ),
+        ));
+        ?>
 
-<p>Please fill out the following form with your login credentials:</p>
+        <p>
+            <?php echo $form->error($model, 'username'); ?>
+            <?php echo $form->error($model, 'password'); ?>
+        </p>
 
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+        <div class="form-group">
+            <?php echo $form->labelEx($model, 'username', array('class' => 'control-label')); ?>
+            <?php echo $form->textField($model, 'username', array('class' => 'form-control')); ?>
+            
+        </div>
+        <div class="form-group">
+            <?php echo $form->labelEx($model, 'password', array('class' => 'control-label')); ?>
+            <?php echo $form->passwordField($model, 'password', array('class' => 'form-control ')); ?>
+            
+            <p class="hint">
+                Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd>.
+            </p>
+        </div>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+        <div class="rememberMe form-group">
+            <?php echo $form->checkBox($model, 'rememberMe'); ?>
+            <?php echo $form->label($model, 'rememberMe'); ?>
+            <?php echo $form->error($model, 'rememberMe'); ?>
+        </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
-
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
+        <div class="form-group">
+        <?php echo CHtml::submitButton('Login', array('class'=>'btn btn-default')); ?>
+        </div>
 
 <?php $this->endWidget(); ?>
-</div><!-- form -->
+    </div><!-- form -->
+</div>
