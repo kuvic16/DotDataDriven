acff004cf6dff0ac11fdf486ceefa0d958a87d4e
Shaiful Islam <kuvic16@gmail.com>
2015-12-23 15:25:39 +0600

    minor changes made

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -19,52 +19,81 @@
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/css/bootstrap-theme.min.css" media="screen, projection">
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/css/main.css" media="screen, projection">
 
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/js/jquery.min.js"></script>
         <script src="<?php echo Yii::app()->request->baseUrl; ?>/assets/js/bootstrap.min.js"></script>
 
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     </head>
 
     <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">DotDataDriven</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <?php
+                    $this->widget('zii.widgets.CMenu', array(
+                        'items' => array(
+                            array('label' => 'Home', 'url' => array('/site/index')),
+                            array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
+                            array('label' => 'Contact', 'url' => array('/site/contact')),
+                            array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                            array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                        ),
+                        'htmlOptions' => array('class' => 'nav navbar-nav'),
+                    ));
+                    if (Yii::app()->user->isGuest) {
+                        ?>
+                        <form class="navbar-form navbar-right">
+                            <div class="form-group">
+                                <input type="text" placeholder="Username: admin" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" placeholder="Password: admin" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-success">Sign in</button>
+                        </form>
+                    <?php } ?>
+                </div>
+            </div>
+        </nav>
 
-        <div class="container" id="page">
-
-
-            <div id="header">
-                <div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-            </div><!-- header -->
-
-            <div id="mainmenu">
-                <?php
-                $this->widget('zii.widgets.CMenu', array(
-                    'items' => array(
-                        array('label' => 'Home', 'url' => array('/site/index')),
-                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
-                        array('label' => 'Contact', 'url' => array('/site/contact')),
-                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
-                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
-                    ),
-                ));
-                ?>
-            </div><!-- mainmenu -->
-            <?php if (isset($this->breadcrumbs)): ?>
-                <?php
-                $this->widget('zii.widgets.CBreadcrumbs', array(
-                    'links' => $this->breadcrumbs,
-                ));
-                ?><!-- breadcrumbs -->
-<?php endif ?>
-
-<?php echo $content; ?>
-
-            <div class="clear"></div>
-
-            <div id="footer">
-                Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-                All Rights Reserved.<br/>
-<?php echo Yii::powered(); ?>
-            </div><!-- footer -->
-
-        </div><!-- page -->
-
+        <?php
+        if ($this->getAction()->getId() == 'index') {
+            ?>
+            <div class="jumbotron">
+                <div class="container">
+                    <h1>Hello, world!</h1>
+                    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+        <div class="container">
+            <?php if ($this->getAction()->getId() != 'index' && isset($this->breadcrumbs)): ?>
+                <ol class="breadcrumb">
+                    <?php
+                    $this->widget('zii.widgets.CBreadcrumbs', array(
+                        'links' => $this->breadcrumbs,
+                    ));
+                    ?>
+                </ol>
+            <?php endif ?>
+            <?php echo $content; ?>
+            <hr>
+            <footer>
+                <p>Copyright &copy; <?php echo date('Y'); ?> by <kbd><a href="http://sheemoul.wordpress.com" style="color: white">Shaiful Islam Palash</a></kbd>. All Rights Reserved. <kbd><?php echo Yii::powered(); ?></kbd>
+                </p>
+            </footer> 
+        </div>
     </body>
 </html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,32 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="row">
+    <div class="col-md-3">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/images/fileupload.png" alt="CSV/XSL file upload" class="img-thumbnail">
+        <h2>CSV/XSL Upload</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div>
+    <div class="col-md-3">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/images/editfile.png" alt="Edit file" class="img-thumbnail">
+        <h2>Change File</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div>
+    <div class="col-md-3">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/images/map.png" alt="..." class="img-thumbnail">
+        <h2>D3 Mapping</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div>
+    <div class="col-md-3">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/images/analysis.png" alt="..." class="img-thumbnail">
+        <h2>D3 Analysis</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div>
+</div>
